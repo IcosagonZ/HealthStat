08e446bc5f83de58c451eeba997cd99f50b66474
IcosagonZ <rohanraj3870z@gmail.com>
2025-09-02 23:05:31 +0530

    Added basic tab implementation

--- lib/main.dart
+++ lib/main.dart
@@ -32,26 +32,79 @@ class Page_Home extends StatefulWidget
 
 class _Page_HomeState extends State<Page_Home>
 {
-
   // Main app UI
   @override
   Widget build(BuildContext context)
   {
+    final color_scheme = Theme.of(context).colorScheme;
+
+    Color color_primary = color_scheme.primary;
+    Color color_secondary = color_scheme.secondary;
+    Color color_onprimary = color_scheme.onPrimary;
+    Color color_onsecondary = color_scheme.onSecondary;
+    Color color_background = color_scheme.onBackground;
+    Color color_surface = color_scheme.onSurface;
+
     return Scaffold
     (
-      appBar: AppBar
+      /*appBar: AppBar
       (
         title: Text(widget.title),
         actions:
         [
 
         ],
-      ),
+      ),*/
       body: SizedBox.expand
       (
-        child: Text
+        child: DefaultTabController
         (
-          "Test"
+          initialIndex: 1,
+          length: 4,
+          child: Column
+          (
+            children:
+            [
+              Container
+              (
+                margin: EdgeInsets.only(top: 16),
+
+                child: TabBar
+                (
+                  tabs:
+                  [
+                    Tab(icon: Icon(Icons.dashboard), text: "Overview"),
+                    Tab(icon: Icon(Icons.event_note), text: "Journal"),
+                    Tab(icon: Icon(Icons.history), text: "History"),
+                    Tab(icon: Icon(Icons.assignment_add), text: "Add"),
+                  ],
+                  labelColor: color_primary,
+                  unselectedLabelColor: color_secondary,
+                ),
+              ),
+              Expanded
+              (
+                child: TabBarView
+                (
+                  children:
+                  [
+                    Center(child:
+                      Text('1')
+                    ),
+                    Center(child:
+                      Text('2')
+                    ),
+                    Center(child:
+                      Text('3')
+                    ),
+                    Center(child:
+                      Text('4')
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
         ),
        ),
     );
