35074d06be3d4f6c4eba78ac58fa28151103ebf0
IcosagonZ <rohanraj3870z@gmail.com>
2025-09-03 23:53:44 +0530

    Added graph view

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:material_symbols_icons/symbols.dart';
 
+import 'package:fl_chart/fl_chart.dart';
+
 void main()
 {
   runApp(const MyApp());
@@ -62,6 +64,51 @@ class _Page_HomeState extends State<Page_Home>
     final style_titlelarge = text_theme.titleLarge;
     final style_titlemedium = text_theme.titleMedium;
     final style_titlesmall = text_theme.titleSmall;
+
+    // Development test data
+    final List<FlSpot> graph_data_calories =
+    [
+      FlSpot(1, 1825),
+      FlSpot(2, 1967),
+      FlSpot(3, 2465),
+      FlSpot(4, 1956),
+      FlSpot(5, 1767),
+      FlSpot(6, 1867),
+      FlSpot(7, 1847),
+    ];
+
+    final List<FlSpot> graph_data_mood =
+    [
+      FlSpot(1, 8),
+      FlSpot(2, 6),
+      FlSpot(3, 5),
+      FlSpot(4, 4),
+      FlSpot(5, 5),
+      FlSpot(6, 3),
+      FlSpot(7, 7),
+    ];
+
+    final List<FlSpot> graph_data_weight =
+    [
+      FlSpot(1, 65),
+      FlSpot(2, 67),
+      FlSpot(3, 68),
+      FlSpot(4, 68),
+      FlSpot(5, 69),
+      FlSpot(6, 67),
+      FlSpot(7, 65),
+    ];
+
+    final List<FlSpot> graph_data_height =
+    [
+      FlSpot(1, 170),
+      FlSpot(2, 170),
+      FlSpot(3, 170),
+      FlSpot(4, 170),
+      FlSpot(5, 170),
+      FlSpot(6, 170),
+      FlSpot(7, 170),
+    ];
 
     return Scaffold
     (
@@ -294,11 +341,49 @@ class _Page_HomeState extends State<Page_Home>
                     Center(child: Padding
                       (
                         padding: EdgeInsets.all(16),
-                        child: Column
+                        child: ListView
                         (
                           children:
                           [
-                            Text("3"),
+                            CardGraph
+                            (
+                              text:"Calories",
+                              graph_data: graph_data_calories,
+                              graph_max_x: 7,
+                              graph_min_x: 1,
+                              graph_max_y: 3000,
+                              graph_min_y: 1000,
+                            ),
+
+                            CardGraph
+                            (
+                              text:"Mood",
+                              graph_data: graph_data_mood,
+                              graph_max_x: 7,
+                              graph_min_x: 1,
+                              graph_max_y: 10,
+                              graph_min_y: 0,
+                            ),
+
+                            CardGraph
+                            (
+                              text:"Weight",
+                              graph_data: graph_data_weight,
+                              graph_max_x: 7,
+                              graph_min_x: 1,
+                              graph_max_y: 70,
+                              graph_min_y: 60,
+                            ),
+
+                            CardGraph
+                            (
+                              text:"Height",
+                              graph_data: graph_data_height,
+                              graph_max_x: 7,
+                              graph_min_x: 1,
+                              graph_max_y: 180,
+                              graph_min_y: 160,
+                            ),
                           ],
                         ),
                        ),
@@ -320,43 +405,6 @@ class _Page_HomeState extends State<Page_Home>
                   ],
                 ),
               ),
-
-              // Bottom section overview
-              /*
-              Container
-              (
-                margin: EdgeInsets.all(16),
-
-                child: Column
-                (
-                  children: [
-                    Text("Calories", style: style_titlelarge),
-                    SizedBox(height: 4),
-
-                    // Progress bar: Stackover flow answer
-                    Stack(
-                      children: <Widget>[
-                        SizedBox(
-                          height: 20,
-                          child: LinearProgressIndicator(
-                            value: 0.6,
-                            backgroundColor: color_background,
-                            valueColor: AlwaysStoppedAnimation<Color>(color_primary),
-                          ),
-                        ),
-                        Align(child: Text("600/1000", style: TextStyle(color: color_onprimary)), alignment: Alignment.topCenter,),
-                      ],
-                    ),
-
-                    SizedBox(height: 16),
-
-                    Text("Overview", style: style_titlelarge),
-                    SizedBox(height: 4),
-                    Text("Looking good", style: style_displaysmall),
-                  ],
-                ),
-              ),
-              */
             ],
           ),
         ),
@@ -414,7 +462,7 @@ class CardJournal extends StatelessWidget
           splashColor: color_onprimary,
           onTap: ()
           {
-            debugPrint("Note tapped");
+            //debugPrint("Note tapped");
           },
           child: Row
           (
@@ -446,6 +494,251 @@ class CardJournal extends StatelessWidget
               ),
             ],
            ),
+        ),
+      ),
+    );
+  }
+}
+
+class CardGraph extends StatelessWidget
+{
+  final String text;
+
+  final List<FlSpot> graph_data;
+
+  final double graph_min_x;
+  final double graph_max_x;
+  final double graph_min_y;
+  final double graph_max_y;
+
+  //final int graph_height;
+
+  const CardGraph({
+    Key? key,
+    required this.text,
+    required this.graph_data,
+
+    required this.graph_min_x,
+    required this.graph_max_x,
+    required this.graph_min_y,
+    required this.graph_max_y,
+  }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context)
+  {
+    // Default theme variables
+    final color_scheme = Theme.of(context).colorScheme;
+    final text_theme = Theme.of(context).textTheme;
+
+    Color color_primary = color_scheme.primary;
+    //Color color_secondary = color_scheme.secondary;
+    Color color_onprimary = color_scheme.onPrimary;
+    //Color color_onsecondary = color_scheme.onSecondary;
+    Color color_background = color_scheme.onBackground;
+    Color color_surface = color_scheme.onSurface;
+
+    //final style_displaylarge = text_theme.displayLarge;
+    //final style_displaymedium = text_theme.displayMedium;
+    //final style_displaysmall = text_theme.displaySmall;
+
+    //final style_headlinelarge = text_theme.headlineLarge;
+    //final style_headlinemedium = text_theme.headlineMedium;
+    final style_headlinesmall = text_theme.headlineSmall;
+
+    final style_titlelarge = text_theme.titleLarge;
+    //final style_titlemedium = text_theme.titleMedium;
+    //final style_titlesmall = text_theme.titleSmall;
+
+    return Center
+    (
+      child: Card
+      (
+        clipBehavior: Clip.hardEdge,
+        child: InkWell
+        (
+          splashColor: color_onprimary,
+          onTap: ()
+          {
+            //debugPrint("Note tapped");
+          },
+          child: Row
+          (
+            children:
+            [
+              Expanded
+              (
+                //width: 300,
+                //height: 100,
+                child: Padding
+                (
+                  padding: EdgeInsets.all(16),
+                  child: Column
+                  (
+                    children:
+                    [
+                      Text("$text", style: TextStyle(color: color_primary)),
+                      SizedBox(height: 16),
+                      GraphChart
+                      (
+                        graph_data: graph_data,
+                        graph_max_x: graph_max_x,
+                        graph_min_x: graph_min_x,
+                        graph_max_y: graph_max_y,
+                        graph_min_y: graph_min_y,
+                        graph_height: 200,
+                      ),
+                    ]
+                  )
+                )
+              ),
+            ],
+           ),
+        ),
+      ),
+    );
+  }
+}
+
+class GraphChart extends StatelessWidget
+{
+  /*
+  final List<FlSpot> graph_data =
+  [
+    FlSpot(1, 65),
+    FlSpot(2, 67),
+    FlSpot(3, 68),
+    FlSpot(4, 63),
+    FlSpot(5, 69),
+  ];
+  */
+
+  final List<FlSpot> graph_data;
+
+  final double graph_min_x;
+  final double graph_max_x;
+  final double graph_min_y;
+  final double graph_max_y;
+
+  final double graph_height;
+
+  const GraphChart({
+    Key? key,
+    required this.graph_data,
+
+    required this.graph_min_x,
+    required this.graph_max_x,
+    required this.graph_min_y,
+    required this.graph_max_y,
+
+    required this.graph_height,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context)
+  {
+    // Default theme variables
+    final color_scheme = Theme.of(context).colorScheme;
+    //final text_theme = Theme.of(context).textTheme;
+
+    Color color_primary = color_scheme.primary;
+    //Color color_secondary = color_scheme.secondary;
+    //Color color_onprimary = color_scheme.onPrimary;
+    //Color color_onsecondary = color_scheme.onSecondary;
+    //Color color_background = color_scheme.onBackground;
+    //Color color_surface = color_scheme.onSurface;
+
+    //final style_displaylarge = text_theme.displayLarge;
+    //final style_displaymedium = text_theme.displayMedium;
+    //final style_displaysmall = text_theme.displaySmall;
+
+    //final style_headlinelarge = text_theme.headlineLarge;
+    //final style_headlinemedium = text_theme.headlineMedium;
+    //final style_headlinesmall = text_theme.headlineSmall;
+
+    //final style_titlelarge = text_theme.titleLarge;
+    //final style_titlemedium = text_theme.titleMedium;
+    //final style_titlesmall = text_theme.titleSmall;
+
+    return SizedBox
+    (
+      //aspectRatio: 1,
+      height: graph_height,
+      child: LineChart
+      (
+        LineChartData
+        (
+          minX: graph_min_x,
+          maxX: graph_max_x,
+          minY: graph_min_y,
+          maxY: graph_max_y,
+
+          gridData: FlGridData(show: true),
+          borderData: FlBorderData
+          (
+            show: false,
+            border: Border.all(color: color_primary, width: 1),
+          ),
+
+          titlesData: FlTitlesData(
+            topTitles: AxisTitles
+            (
+              sideTitles: SideTitles
+              (
+                showTitles: false
+              ),
+            ),
+            bottomTitles: AxisTitles
+            (
+              sideTitles: SideTitles
+              (
+                showTitles: true,
+                reservedSize: 30,
+                interval: 1,
+              ),
+            ),
+
+            leftTitles: AxisTitles
+            (
+              sideTitles: SideTitles
+              (
+                showTitles: true,
+                reservedSize: 50,
+                //interval: 1,
+              ),
+            ),
+
+            rightTitles: AxisTitles
+            (
+              sideTitles: SideTitles
+              (
+                showTitles: true,
+                reservedSize: 50,
+                getTitlesWidget: (value, meta)
+                {
+                  return Text("");
+                },
+              ),
+            ),
+          ),
+
+        lineBarsData:
+        [
+          LineChartBarData
+          (
+            spots: graph_data,
+            isCurved: true,
+            barWidth: 3,
+            color: color_primary,
+            dotData: FlDotData(show: true),
+            belowBarData: BarAreaData
+            (
+              show: false,
+              color: color_primary,
+            ),
+          ),
+        ],
         ),
       ),
     );
